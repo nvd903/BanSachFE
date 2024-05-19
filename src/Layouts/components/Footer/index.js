d75f0e37baa2df97ef24d388a6d9c615afc524a9
dung021102 <dung02112222@gmail.com>
2024-05-19 15:32:13 +0700

    add footer

--- src/Layouts/components/Footer/index.js
+++ src/Layouts/components/Footer/index.js
@@ -1,5 +1,30 @@
+import "./Footer.scss";
+
+import logo from "../../../assets/images/logo.png"
+
 function Footer() {
-  return <div className="container-fluid">Footer</div>;
+  return <div className="container-fluid">
+      <p>
+        Copyright © By Nguyễn Văn Dũng.
+      </p>
+      <div className="footer_left">
+        <div className="footer_contact">
+          <p>Powered by</p> 
+          <a
+            href="https://1.envato.market/tf-merkulove"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="footer_logo"
+              src={logo}
+              alt="ThreeCents"
+            />
+          </a>
+        </div>
+        <p> Liên hệ: 19006868</p>
+      </div>
+  </div>;
 }
 
 export default Footer;
